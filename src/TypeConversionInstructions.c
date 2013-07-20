271ebbce88c834d0432ca2c066826da948965235
renatacmn <cmn.renata@gmail.com>
2013-07-20 00:13:24 -0300

    TypeConversionInstructions completo

--- src/TypeConversionInstructions.c
+++ src/TypeConversionInstructions.c
@@ -2,62 +2,139 @@
 
 /*0x85*/
 int i2l(Interpretador* interpretador) {
+    Operand operand1, operand_result;
+    operand1 = popOperand(&(interpretador->topStackFrame->frame->topOperand));
+    operand_result.operandType.longType = operand1.operandType.intType;
+    pushOperand(&(interpretador->topStackFrame->frame->topOperand), operand_result);
+	return 0;
 }
 
 /*0x86*/
 int i2f(Interpretador* interpretador) {
+    Operand operand1, operand_result;
+    operand1 = popOperand(&(interpretador->topStackFrame->frame->topOperand));
+    operand_result.operandType.floatType = (float) operand1.operandType.intType;
+    pushOperand(&(interpretador->topStackFrame->frame->topOperand), operand_result);
+	return 0;
 }
 
 /*0x87*/
 int i2d(Interpretador* interpretador) {
+    Operand operand1, operand_result;
+    operand1 = popOperand(&(interpretador->topStackFrame->frame->topOperand));
+    operand_result.operandType.doubleType = (double) operand1.operandType.intType;
+    pushOperand(&(interpretador->topStackFrame->frame->topOperand), operand_result);
+	return 0;
 }
 
 /*0x88*/
 int l2i(Interpretador* interpretador) {
+    Operand operand1, operand_result;
+    operand1 = popOperand(&(interpretador->topStackFrame->frame->topOperand));
+    operand_result.operandType.intType = (int) operand1.operandType.longType;
+    pushOperand(&(interpretador->topStackFrame->frame->topOperand), operand_result);
+	return 0;
 }
 
 /*0x89*/
 int l2f(Interpretador* interpretador) {
+    Operand operand1, operand_result;
+    operand1 = popOperand(&(interpretador->topStackFrame->frame->topOperand));
+    operand_result.operandType.floatType = (float) operand1.operandType.longType;
+    pushOperand(&(interpretador->topStackFrame->frame->topOperand), operand_result);
+	return 0;
 }
 
 /*0x8A*/
 int l2d(Interpretador* interpretador) {
+    Operand operand1, operand_result;
+    operand1 = popOperand(&(interpretador->topStackFrame->frame->topOperand));
+    operand_result.operandType.doubleType = (double) operand1.operandType.longType;
+    pushOperand(&(interpretador->topStackFrame->frame->topOperand), operand_result);
+	return 0;
 }
 
 /*0x8B*/
 int f2i(Interpretador* interpretador) {
+    Operand operand1, operand_result;
+    operand1 = popOperand(&(interpretador->topStackFrame->frame->topOperand));
+    operand_result.operandType.intType = (int) operand1.operandType.floatType;
+    pushOperand(&(interpretador->topStackFrame->frame->topOperand), operand_result);
+	return 0;
 }
 
 /*0x8C*/
 int f2l(Interpretador* interpretador) {
+    Operand operand1, operand_result;
+    operand1 = popOperand(&(interpretador->topStackFrame->frame->topOperand));
+    operand_result.operandType.longType = (long) operand1.operandType.floatType;
+    pushOperand(&(interpretador->topStackFrame->frame->topOperand), operand_result);
+	return 0;
 }
 
 /*0x8D*/
 int f2d(Interpretador* interpretador) {
+    Operand operand1, operand_result;
+    operand1 = popOperand(&(interpretador->topStackFrame->frame->topOperand));
+    operand_result.operandType.doubleType = (double) operand1.operandType.floatType;
+    pushOperand(&(interpretador->topStackFrame->frame->topOperand), operand_result);
+	return 0;
 }
 
 /*0x8E*/
 int d2i(Interpretador* interpretador) {
+    Operand operand1, operand_result;
+    operand1 = popOperand(&(interpretador->topStackFrame->frame->topOperand));
+    operand_result.operandType.intType = (int) operand1.operandType.doubleType;
+    pushOperand(&(interpretador->topStackFrame->frame->topOperand), operand_result);
+	return 0;
 }
 
 /*0x8F*/
 int d2l(Interpretador* interpretador) {
+    Operand operand1, operand_result;
+    operand1 = popOperand(&(interpretador->topStackFrame->frame->topOperand));
+    operand_result.operandType.longType = (long) operand1.operandType.doubleType;
+    pushOperand(&(interpretador->topStackFrame->frame->topOperand), operand_result);
+	return 0;
 }
 
 /*0x90*/
 int d2f(Interpretador* interpretador) {
+    Operand operand1, operand_result;
+    operand1 = popOperand(&(interpretador->topStackFrame->frame->topOperand));
+    operand_result.operandType.floatType = (float) operand1.operandType.doubleType;
+    pushOperand(&(interpretador->topStackFrame->frame->topOperand), operand_result);
+	return 0;
 }
 
 /*0x91*/
 int i2b(Interpretador* interpretador) {
+    Operand operand1, operand_result;
+    u1 aux;
+    operand1 = popOperand(&(interpretador->topStackFrame->frame->topOperand));
+    u1 = operand1.operandType.intType;
+    operand_result.operandType.intType = (int) u1;
+    pushOperand(&(interpretador->topStackFrame->frame->topOperand), operand_result);
+	return 0;
 }
 
 /*0x92*/
 int i2c(Interpretador* interpretador) {
+    Operand operand1, operand_result;
+    operand1 = popOperand(&(interpretador->topStackFrame->frame->topOperand));
+    operand_result.operandType.intType = (char) operand1.operandType.intType;
+    pushOperand(&(interpretador->topStackFrame->frame->topOperand), operand_result);
+	return 0;
 }
 
 /*0x93*/
 int i2s(Interpretador* interpretador) {
+    Operand operand1, operand_result;
+    operand1 = popOperand(&(interpretador->topStackFrame->frame->topOperand));
+    operand_result.operandType.intType = (short) operand1.operandType.intType;
+    pushOperand(&(interpretador->topStackFrame->frame->topOperand), operand_result);
+	return 0;
 }
 
 #endif
--- src/TypeConversionInstructions.h
+++ src/TypeConversionInstructions.h
@@ -1,20 +1,49 @@
 #ifndef TYPECONVERSIONINSTRUCTIONS_H
 #define TYPECONVERSIONINSTRUCTIONS_H
 
+/** Converte o primeiro valor da pilha de int para long */
 int i2l(Interpretador*); /*0x85*/
+
+/** Converte o primeiro valor da pilha de int para float */
 int i2f(Interpretador*); /*0x86*/
+
+/** Converte o primeiro valor da pilha de int para double */
 int i2d(Interpretador*); /*0x87*/
+
+/** Converte o primeiro valor da pilha de long para int */
 int l2i(Interpretador*); /*0x88*/
+
+/** Converte o primeiro valor da pilha de long para float */
 int l2f(Interpretador*); /*0x89*/
+
+/** Converte o primeiro valor da pilha de long para double */
 int l2d(Interpretador*); /*0x8A*/
+
+/** Converte o primeiro valor da pilha de float para int */
 int f2i(Interpretador*); /*0x8B*/
+
+/** Converte o primeiro valor da pilha de float para long */
 int f2l(Interpretador*); /*0x8C*/
+
+/** Converte o primeiro valor da pilha de float para double */
 int f2d(Interpretador*); /*0x8D*/
+
+/** Converte o primeiro valor da pilha de double para int */
 int d2i(Interpretador*); /*0x8E*/
+
+/** Converte o primeiro valor da pilha de double para long */
 int d2l(Interpretador*); /*0x8F*/
+
+/** Converte o primeiro valor da pilha de double para float */
 int d2f(Interpretador*); /*0x90*/
+
+/** Converte o primeiro valor da pilha de int para byte */
 int i2b(Interpretador*); /*0x91*/
+
+/** Converte o primeiro valor da pilha de int para char */
 int i2c(Interpretador*); /*0x92*/
+
+/** Converte o primeiro valor da pilha de int para short */
 int i2s(Interpretador*); /*0x93*/
 
 #endif
